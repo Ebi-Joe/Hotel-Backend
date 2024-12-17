9fdabdfa1499d33ed9ea9f84853478e064952fb7
Ebi-Joe <esukujosiah2006@gmail.com>
2024-12-17 21:53:24 +0100

    Protecn

--- routers/conciergeRouters.js
+++ routers/conciergeRouters.js
@@ -1,15 +1,14 @@
 const conciergeControllers = require('../controllers/conciergeControllers');
 const express = require('express');
 const router = express.Router();
-const { auth, admin } = require('../middleware/auth')
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
 const uploads = multer({ storage:storage });
 
-router.post("/api/newConcierge", admin, uploads.array("img", 1), conciergeControllers.newConcierge)
+router.post("/api/newConcierge", uploads.array("img", 1), conciergeControllers.newConcierge)
 router.get("/api/getOne-concierge", conciergeControllers.getOneConcierge)
-router.get("/api/getAll-concierges", admin, conciergeControllers.getAllConcierge)
+router.get("/api/getAll-concierges", conciergeControllers.getAllConcierge)
 router.delete("/api/deleteOne-concierge", conciergeControllers.deleteOneConcierge)
 router.patch("/api/updateOne-concierge", conciergeControllers.updateOneConcierge)
 
--- routers/roomTypeRouters.js
+++ routers/roomTypeRouters.js
@@ -1,13 +1,12 @@
 const roomTypeControllers = require('../controllers/roomTypeControllers');
 const express = require('express');
 const router = express.Router()
-const { admin } = require('../middleware/auth')
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
 const uploads = multer({ storage:storage });
 
-router.post("/api/new-roomType", admin, uploads.array("img", 10), roomTypeControllers.createRoomType),
+router.post("/api/new-roomType", uploads.array("img", 10), roomTypeControllers.createRoomType),
 router.get("/api/getOne-roomType", roomTypeControllers.getOneRoomType),
 router.get("/api/getAll-roomType", roomTypeControllers.getAllRoomTypes),
 router.delete("/api/deleteOne-roomType", roomTypeControllers.deleteOneRoomType),
