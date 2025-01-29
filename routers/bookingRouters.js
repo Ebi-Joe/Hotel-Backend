0b4348e8a7bd5229178cfb36e189216df564c2bd
Ebi-Joe <esukujosiah2006@gmail.com>
2025-01-29 14:44:02 +0100

    Vercel Progres

--- routers/bookingRouters.js
+++ routers/bookingRouters.js
@@ -3,8 +3,8 @@ const express = require('express');
 const { auth } = require('../middleware/auth')
 const router = express.Router()
 
-router.post("/api/booking", auth, bookingControllers.newBooking)
-router.post("/api/payment/verify", auth, bookingControllers.verifyPayments)
+router.post("/api/booking", bookingControllers.newBooking)
+router.post("/api/payment/verify", bookingControllers.verifyPayments)
 router.get("/api/getAll-Bookings",bookingControllers.getAllBookings)
 
 module.exports = router
